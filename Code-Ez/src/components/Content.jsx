f6028c7c0825d9afa915856c3d3f1b6b794fb6ef
Chaitanya-idk <neeli.sai15106@gmail.com>
2026-02-15 23:10:28 +0530

    Basic Landing Page Front end is done

---
+++ Code-Ez/src/components/Content.jsx
@@ -0,0 +1,20 @@
+
+import './Content.css';
+export function Content() {
+  return (
+    <>
+    
+      <section className="about">
+        <h1 style={{fontSize :"2.5rem"}}>Don't Fear STS anymore with CodeEz</h1>
+        <p style={{marginTop :"2rem", marginBottom :"2rem"}}>
+          Many Students face problems writing code directly during the exam days
+          having no confidence. So we have developed a website to check and
+          practice your STS questions along with Test Cases.
+        </p>
+
+        <button>STS 3007</button>
+      </section>
+      
+    </>
+  );
+}
---
+++ Code-Ez/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css'
+export function Footer(){
+    return (
+        <div className="footer">
+            <div className="left">
+                <p><a>Find me on LinedIn</a></p>
+            </div>
+            <div className="middle">
+                <p>Made with '❤️'</p>
+            <p><a>Consider Star-ing My Repo</a></p>
+            </div>
+            <div className="right">
+                <p><a>Email</a></p>
+                <p><a>Instagram</a></p>
+            </div>
+        </div>
+    );
+}
